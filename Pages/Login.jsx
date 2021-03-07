eddbb94d7d04e83d6e2f8047c7328f46e006fad2
vinayaksh42 <vinayaksh42@gmail.com>
2021-03-07 13:13:32 +0530

    eslint

--- Pages/Login.jsx
+++ Pages/Login.jsx
@@ -1,5 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+/* eslint-disable no-shadow */
+/* eslint-disable no-use-before-define */
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,16 +10,16 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
-} from "react-native";
- 
+} from 'react-native';
+
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
- 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={require("../assets/favicon.png")} />
- 
+      <Image style={styles.image} source={require('../assets/favicon.png')} />
+
       <StatusBar style="auto" />
       <View style={styles.inputView}>
         <TextInput
@@ -27,69 +29,69 @@ export default function App() {
           onChangeText={(email) => setEmail(email)}
         />
       </View>
- 
+
       <View style={styles.inputView}>
         <TextInput
           style={styles.TextInput}
           placeholder="Password."
           placeholderTextColor="#003f5c"
-          secureTextEntry={true}
+          secureTextEntry
           onChangeText={(password) => setPassword(password)}
         />
       </View>
- 
+
       <TouchableOpacity>
         <Text style={styles.forgot_button}>Forgot Password?</Text>
       </TouchableOpacity>
- 
+
       <TouchableOpacity style={styles.loginBtn}>
         <Text style={styles.loginText}>LOGIN</Text>
       </TouchableOpacity>
     </View>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
- 
+
   image: {
     marginBottom: 40,
   },
- 
+
   inputView: {
-    backgroundColor: "#FFC0CB",
+    backgroundColor: '#FFC0CB',
     borderRadius: 30,
-    width: "70%",
+    width: '70%',
     height: 45,
     marginBottom: 20,
- 
-    alignItems: "center",
+
+    alignItems: 'center',
   },
- 
+
   TextInput: {
     height: 50,
     flex: 1,
     padding: 10,
     marginLeft: 20,
   },
- 
+
   forgot_button: {
     height: 30,
     marginBottom: 30,
   },
- 
+
   loginBtn: {
-    width: "80%",
+    width: '80%',
     borderRadius: 25,
     height: 50,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     marginTop: 40,
-    backgroundColor: "#FF1493",
+    backgroundColor: '#FF1493',
   },
 });
